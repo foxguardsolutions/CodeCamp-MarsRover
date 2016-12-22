ec345aded56bf3652f6acfbf05fdb03066f2bb5b
Phil Wright <pwright@foxguardsolutions.com>
2016-12-22 15:32:02 -0500

    Created an Adaptor that rotates a Rover when executing commands

---
+++ MarsRover/MarsRover.Tests/AdaptorTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class AdaptorTests
+    {
+        [TestCaseSource(nameof(RotateTestCases))]
+        public void RotateRover_ChangesOrientation(char initialDirection, char command, Type expectedOrientation)
+        {
+            var initializer = new Initializer();
+            var rover = initializer.PlaceRover(0, 0, initialDirection, new Grid());
+            var adaptor = new Adaptor(rover);
+            adaptor.Execute(command);
+            var endOrientation = rover.GetOrientation();
+            Assert.That(endOrientation, Is.EqualTo(expectedOrientation));
+        }
+
+        private static IEnumerable<TestCaseData> RotateTestCases()
+        {
+            yield return new TestCaseData('N', 'l', typeof(FacingWest));
+            yield return new TestCaseData('N', 'r', typeof(FacingEast));
+            yield return new TestCaseData('N', 'f', typeof(FacingNorth));
+            yield return new TestCaseData('W', 'l', typeof(FacingSouth));
+        }
+    }
+}
---
+++ MarsRover/MarsRover/Adaptor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class Adaptor
+    {
+        private Rover _rover;
+
+        public Adaptor(Rover rover)
+        {
+            _rover = rover;
+        }
+
+        public void Execute(char command)
+        {
+            if (command == 'l')
+                _rover.Rotate(true);
+            else if (command == 'r')
+                _rover.Rotate(false);
+        }
+    }
+}

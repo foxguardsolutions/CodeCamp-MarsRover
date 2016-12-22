653d51b8f07cce7cbd54e649e07f44246f2d269a
Phil Wright <pwright@foxguardsolutions.com>
2016-12-22 13:57:43 -0500

    Made an Initializer class to place a Rover on a Grid

---
+++ MarsRover/MarsRover.Tests/InitializerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class InitializerTests
+    {
+        [Test]
+        public void PlaceRover_GivenValidPlacement_ReturnsRoverWithInitialValues()
+        {
+            var initializer = new Initializer();
+            var rover = initializer.PlaceRover(0, 0, 'N', new Grid());
+            var coordinates = rover.GetLocation().Coordinates;
+            Assert.That(coordinates, Is.EqualTo(new int[] { 0, 0 }));
+        }
+    }
+}
---
+++ MarsRover/MarsRover/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MarsRover
+{
+    public class Initializer
+    {
+        public Rover PlaceRover(int x, int y, char initialDirection, Grid grid)
+        {
+            var startingOrientation = new FacingNorth();
+            return new Rover(x, y, startingOrientation, grid);
+        }
+    }
+}

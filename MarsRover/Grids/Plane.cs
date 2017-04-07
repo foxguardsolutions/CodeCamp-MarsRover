3489e75f16da304426b22d78e490db68426edbb9
Taylor Bittner <tbittner@foxguardsolutions.com>
2017-04-07 16:52:11 -0400

    Planes can have obstacles
    A grid can contain a list of coordinates where obstacles are located.
    Moving into one of those locations will return the position to it's
    previous location and throw an error.

---
+++ MarsRover/Grids/Plane.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MarsRover.Grids
+{
+    public class Plane
+    {
+        public IGrid Grid { get; set; }
+        public IEnumerable<Coordinates> Obstacles { get; set; }
+
+        public Plane(IGrid grid, IEnumerable<Coordinates> obstacles)
+        {
+            Grid = grid;
+            Obstacles = obstacles;
+        }
+    }
+}
